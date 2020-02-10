13da2c486ee65f842a81035b52e74dc95aea3534
Jaydieboy <hecktwin@gmail.com>
2020-02-09 22:21:35 -0800

    GETs index.gtpl via the server

--- fileServer_test.go
+++ fileServer_test.go
@@ -14,10 +14,24 @@ func TestGETUploadPage(t *testing.T) {
 		SpecServer(response, request)
 
 		got := response.Body.String()
-		want := "test"
+		want := uploadText
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
 }
+
+const uploadText = `<!DOCTYPE html>
+<html>
+<head>
+<title>Upload File</title>
+</head>
+<body>
+<form enctype="multipart/form-data" action="http://localhost:5000/upload/" method="post">
+<input type="file" name="uploadfile" id="">
+<input type="hidden" name="token" value="">
+<input type="submit" value="upload">
+</form>
+</body>
+</html>`
--- fileserver.go
+++ fileserver.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 // SpecServer returns "test" upon request.
 func SpecServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "test")
+	fmt.Println("method:", r.Method)
+	t, _ := template.ParseFiles("index.gtpl")
+	t.Execute(w, nil)
 }
